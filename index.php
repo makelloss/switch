b8cd44986a610f3981c9a56ab35e4880d2e9d279
makelloss <artemvakhno@gmail.com>
2022-09-18 14:59:34 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$mark = 2;
+switch ($mark) {
+    case 2:echo "Оценка 1";
+        break;
+    case 2:echo "Оценка 2";
+        break;
+        case 3:echo "Оценка 3";
+        break;
+    case 4:echo "Оценка 4";
+        break;
+    case 5:echo "Оценка 5";
+        break;
+    default:echo "Такой оценки нету";
+}
