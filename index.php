538aec1996820834792cd836dfc6763e5776e382
makelloss <artemvakhno@gmail.com>
2022-09-18 15:13:08 +0300

    +case 12

--- index.php
+++ index.php
@@ -1,15 +1,29 @@
 <?php
-$mark = rand (0,6);
+$mark = rand (0,15);
 switch ($mark) {
-    case 1:echo "Оценка $mark";
+    case 1:echo "Оценка $mark. Исрпавишь";
         break;
-    case 2:echo "Оценка $mark";
+    case 2:echo "Оценка $mark. Исрпавишь";
         break;
-    case 3:echo "Оценка $mark";
+    case 3:echo "Оценка $mark. Исрпавишь";
         break;
-    case 4:echo "Оценка $mark";
+    case 4:echo "Оценка $mark. Исрпавишь";
         break;
-    case 5:echo "Оценка $mark";
+    case 5:echo "Оценка $mark. Исрпавишь";
+        break;
+    case 6:echo "Оценка $mark. Исрпавишь";
+        break;
+    case 7:echo "Оценка $mark. Исрпавишь";
+        break;
+    case 8:echo "Оценка $mark. Молодец";
+        break;
+    case 9:echo "Оценка $mark. Молодец";
+        break;
+    case 10:echo "Оценка $mark. Молодец";
+        break;
+    case 11:echo "Оценка $mark. Молодец";
+        break;
+    case 12:echo "Оценка $mark. Молодец";
         break;
     default:echo "<span style = \" color: red;\">Такой оценки нету ($mark) </span>";
 }
